7a8a1ee15c9dd5e871c4e68885440ad75282074c
Haile, Aberham <hailea3@nationwide.com>
2019-04-04 13:56:41 -0400

    canary figured out finally kinda, locked down some routes, also removed asset JUNK

--- lib/metro/abilities.ex
+++ lib/metro/abilities.ex
@@ -1,11 +1,13 @@
 alias Metro.Account.{User, Card}
 
 defimpl Canada.Can, for: User do
+
+  def can?(%User{id: user_id_1, is_librarian?: true}, _, _), do: true
+
   def can?(%User{id: user_id_1}, action, %User{id: user_id_2})
-      when action in [:edit, :update, :delete] do
+      when action in [:show, :edit, :update, :delete] do
       user_id_1 == user_id_2
   end
 
-
-  def can?(_, _, _), do: true
+  def can?(%User{id: user_id}, _, _), do: false
 end
